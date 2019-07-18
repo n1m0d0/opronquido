d4e6ffc76e6e244165d4915949b6f132057d0321
n1m0d0 <vascojohn@gmail.com>
2019-07-18 13:28:46 -0400

    servicios

--- app/src/main/java/ajatic/com/snoreop/MainActivity.java
+++ app/src/main/java/ajatic/com/snoreop/MainActivity.java
@@ -1,28 +1,167 @@
 package ajatic.com.snoreop;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
 
-    private final int duration = 2000;
+    private final int duration = 5000;
     Intent ir;
+    Cursor cur;
+    Toast msj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        validarPermisos();
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                ir = new Intent(MainActivity.this, MainActivity.class);
+
+                bd conexion = new bd(MainActivity.this);
+                try {
+                    conexion.abrir();
+                    cur = conexion.sensibilidad();
+                    if (cur.moveToFirst() == false) {
+
+                        conexion.llenar_sensibilidad();
+                        conexion.llenar_tiempoInicial();
+                        conexion.llenar_configuracion();
+                        Log.w("hola", "hoola");
+                    }
+                    conexion.cerrar();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    conexion.cerrar();
+                }
+
+                /*ir = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(ir);
-                finish();
+                finish();*/
             }
         }, duration);
+
+    }
+
+    /******************/
+    private boolean validarPermisos(){
+
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+
+            return true;
+
+        }
+        if((checkSelfPermission(ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) && (checkSelfPermission(ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) && (checkSelfPermission(CAMERA) == PackageManager.PERMISSION_GRANTED) && (checkSelfPermission(WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) && (checkSelfPermission(RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)){
+
+            return true;
+
+        }
+
+        if ((shouldShowRequestPermissionRationale(ACCESS_FINE_LOCATION)) || (shouldShowRequestPermissionRationale(ACCESS_COARSE_LOCATION)) || (shouldShowRequestPermissionRationale(CAMERA)) || (shouldShowRequestPermissionRationale(WRITE_EXTERNAL_STORAGE))  || (shouldShowRequestPermissionRationale(RECORD_AUDIO))) {
+
+            cargardialogo();
+
+        }
+        else {
+
+            requestPermissions(new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION, WRITE_EXTERNAL_STORAGE, CAMERA, RECORD_AUDIO}, 100);
+
+        }
+
+        return false;
+
+    }
+
+    private void cargardialogo(){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Permisos Desactivados");
+        builder.setMessage("Debe aceptar los permisos para el correcto funcionamiento de la App");
+
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION, WRITE_EXTERNAL_STORAGE, CAMERA, RECORD_AUDIO}, 100);
+                }
+
+            }
+        });
+        builder.show();
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(requestCode == 100) {
+
+            if(grantResults.length == 5 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED && grantResults[2] == PackageManager.PERMISSION_GRANTED && grantResults[3] == PackageManager.PERMISSION_GRANTED  && grantResults[4] == PackageManager.PERMISSION_GRANTED) {
+
+
+
+            } else {
+
+                cargardialogo2();
+
+            }
+
+        }
+
+    }
+
+    private void cargardialogo2(){
+
+        final CharSequence[] op = {"si", "no"};
+        final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("Desea configurar los permisos manualmente?");
+        builder.setItems(op, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if(op[which].equals("si")){
+
+                    Intent intent = new Intent();
+                    intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    Uri uri = Uri.fromParts("package", getPackageName(), null);
+                    intent .setData(uri);
+                    startActivity(intent);
+
+                }
+                else {
+
+                    msj = Toast.makeText(MainActivity.this, "los permisos no fueron aceptados", Toast.LENGTH_LONG);
+                    msj.show();
+                    dialog.dismiss();
+
+                }
+
+            }
+        });
+        builder.show();
 
     }
 }
--- app/src/main/java/ajatic/com/snoreop/bd.java
+++ app/src/main/java/ajatic/com/snoreop/bd.java
@@ -1,6 +1,9 @@
 package ajatic.com.snoreop;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -118,6 +121,151 @@ public class bd {
         // TODO Auto-generated method stub
 
         nHelper.close();
+
+    }
+
+    //funciones
+    //sensibilidad
+    //devuelve todos los registros de la tabla sensibilidad
+    public Cursor sensibilidad() throws SQLException {
+        String selectQuery = "SELECT * FROM " + sensibilidad;
+        Cursor cursor = nBD.rawQuery(selectQuery, null);
+        return cursor;
+
+    }
+
+    //registra la sensibilidad
+    public long registrar_sensibilidad(String porcentaje)
+            throws SQLException {
+        // TODO Auto-generated method stub
+        ContentValues cv = new ContentValues();
+        cv.put(this.porcentaje, porcentaje);
+        return nBD.insert(sensibilidad, null, cv);
+
+    }
+    //llenar sensibilidad
+    public void llenar_sensibilidad()
+            throws SQLException {
+        // TODO Auto-generated method stub
+        ContentValues cv1 = new ContentValues();
+        cv1.put(porcentaje, "5%");
+        nBD.insert(sensibilidad, null, cv1);
+        ContentValues cv2 = new ContentValues();
+        cv2.put(porcentaje, "10%");
+        nBD.insert(sensibilidad, null, cv2);
+        ContentValues cv3 = new ContentValues();
+        cv3.put(porcentaje, "15%");
+        nBD.insert(sensibilidad, null, cv3);
+        ContentValues cv4 = new ContentValues();
+        cv4.put(porcentaje, "20%");
+        nBD.insert(sensibilidad, null, cv4);
+
+    }
+    //tiempo inicial
+    //devuelve todos los registros de la tabla tiempoInicial
+    public Cursor tiempoInicial() throws SQLException {
+        String selectQuery = "SELECT * FROM " + tiempoInicial;
+        Cursor cursor = nBD.rawQuery(selectQuery, null);
+        return cursor;
+
+    }
+
+    //registra tiempoInicial
+    public long registrar_tiempoInicial(String tiempo)
+            throws SQLException {
+        // TODO Auto-generated method stub
+        ContentValues cv = new ContentValues();
+        cv.put(this.tiempo, tiempo);
+        return nBD.insert(sensibilidad, null, cv);
+
+    }
+    //llenar tiempoInicial
+    public void llenar_tiempoInicial()
+            throws SQLException {
+        // TODO Auto-generated method stub
+        ContentValues cv1 = new ContentValues();
+        cv1.put(tiempo, "20");
+        nBD.insert(tiempoInicial, null, cv1);
+        ContentValues cv2 = new ContentValues();
+        cv2.put(tiempo, "25");
+        nBD.insert(tiempoInicial, null, cv2);
+        ContentValues cv3 = new ContentValues();
+        cv3.put(tiempo, "30");
+        nBD.insert(tiempoInicial, null, cv3);
+        ContentValues cv4 = new ContentValues();
+        cv4.put(tiempo, "35");
+        nBD.insert(tiempoInicial, null, cv4);
+        ContentValues cv5 = new ContentValues();
+        cv5.put(tiempo, "40");
+        nBD.insert(tiempoInicial, null, cv5);
+
+    }
+
+    //configuracion
+    //devuelve todos los registros de la tabla configuracion
+    public Cursor configuracion() throws SQLException {
+        String selectQuery = "SELECT * FROM " + tiempoInicial;
+        Cursor cursor = nBD.rawQuery(selectQuery, null);
+        return cursor;
+
+    }
+
+    //llenar configuracion
+    public void llenar_configuracion()
+            throws SQLException {
+        // TODO Auto-generated method stub
+        ContentValues cv1 = new ContentValues();
+        cv1.put(sensibilidadId, 1);
+        cv1.put(tiempoInicialId, 1);
+        nBD.insert(configuracion, null, cv1);
+
+    }
+    //actualizar configuracion
+    public void modificar_configuracion(String sensibilidadId, String tiempoInicialId) throws SQLException {
+        // TODO Auto-generated method stub
+
+        ContentValues cv = new ContentValues();
+        cv.put(this.sensibilidadId, sensibilidadId);
+        cv.put(this.tiempoInicialId, tiempoInicialId);
+        nBD.update(configuracion, cv, idConfiguracion + "='" + 1 + "'", null);
+
+    }
+    //promedio
+    //devuelve todos los registros de la tabla promedio
+    public Cursor promedio() throws SQLException {
+        String selectQuery = "SELECT * FROM " + promedio;
+        Cursor cursor = nBD.rawQuery(selectQuery, null);
+        return cursor;
+
+    }
+
+    //registra la sensibilidad
+    public long registrar_promedio(String dia, String promedio)
+            throws SQLException {
+        // TODO Auto-generated method stub
+        ContentValues cv = new ContentValues();
+        cv.put(this.dia, dia);
+        cv.put(this.promedio, promedio);
+        return nBD.insert(promedioDia, null, cv);
+
+    }
+    //monitoreo
+    //devuelve todos los registros de la tabla monitoreo
+    public Cursor monitoreo() throws SQLException {
+        String selectQuery = "SELECT * FROM " + monitoreo;
+        Cursor cursor = nBD.rawQuery(selectQuery, null);
+        return cursor;
+
+    }
+
+    //registra la monitoreo
+    public long registrar_monitoreo(int promedioId, String medicion)
+            throws SQLException {
+        // TODO Auto-generated method stub
+        ContentValues cv = new ContentValues();
+        cv.put(this.promedioId, promedioId);
+        cv.put(this.medicion, medicion);
+        return nBD.insert(monitoreo, null, cv);
 
     }
 
